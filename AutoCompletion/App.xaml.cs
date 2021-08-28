932f66122fc2589dedce6f8433c2e287af724960
autodotua <autodotua@outlook.com>
2021-08-28 15:45:03 +0800

    修复了自动启动设置无法保留的BUG

--- AutoCompletion/App.xaml.cs
+++ AutoCompletion/App.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,15 +19,42 @@ namespace AutoCompletion
 
         private async void ApplicationStartup(object sender, StartupEventArgs e)
         {
+            UnhandledException.RegistAll();
+            UnhandledException.UnhandledExceptionCatched += UnhandledException_UnhandledExceptionCatched;
+
             if (await s.CheckAndOpenWindow<MainWindow>(this))
             {
                 Shutdown();
                 return;
             }
+            if (e.Args.Length == 1 && e.Args[0] == "startup")
+            {
+                FzLib.Program.App.SetWorkingDirectoryToAppPath();
+            }
             MainWindow = new MainWindow();
             if (!(e.Args.Length == 1 && e.Args[0] == "startup"))
             {
                 MainWindow.Show();
+            }
+        }
+
+        private void UnhandledException_UnhandledExceptionCatched(object sender, FzLib.Program.Runtime.UnhandledExceptionEventArgs e)
+        {
+            try
+            {
+                File.AppendAllLines("errors", new string[]
+                {
+                    DateTime.Now.ToString(),
+                    e.Exception.ToString()
+                });
+                MessageBox.Show(e.Exception.Message, "程序发生错误", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch
+            {
+            }
+            finally
+            {
+                Environment.Exit(-1);
             }
         }
     }
